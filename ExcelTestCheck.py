40220b47f4e3350955cce77857261a7e8bdf75b6
yanagi-ori <yanagiori@gmail.com>
2019-04-04 14:03:36 +0400

    Initial commit

---
+++ ExcelTestCheck.py
@@ -0,0 +1,22 @@
+import pyexcel
+import xlwt
+
+input("Для работы программы необходимы таблицы test.xlsx и ans.xlsx в папке с исполнительным файлом"
+      "\n(Нажмите Enter)")
+
+my_array = pyexcel.get_array(file_name="test.xlsx")
+ans = pyexcel.get_array(file_name="ans.xlsx")
+book = xlwt.Workbook(encoding="utf-8")
+results = book.add_sheet("Результаты")
+iq = book.add_sheet("IQ")
+
+for row in range(len(my_array)):
+    total = 0
+    for i in range(len(my_array[row])):
+        if str(my_array[row][i]).lower().replace(" ", '') == str(ans[i][0]):
+            total += 1
+    results.write(row, 0, total)
+    iq.write(row, 0, 75 + 2.5 * total)
+
+book.save("results.xls")
+print("Результат программы сохранен в файл results.xls")
---
+++ install_dependencies.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.Popen("pip install pyexcel", shell=False)
+subprocess.Popen("pip install xlwt", shell=False)
+subprocess.Popen("pip install pyexcel-xls", shell=False)
+subprocess.Popen("pip install pyexcel-xlsx", shell=False)
