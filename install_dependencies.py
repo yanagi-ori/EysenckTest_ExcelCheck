6a33e42271b5ff47cf2bfa14e7a2a5c110f433ed
Ivan Bogatyrev <45294855+yanagi-ori@users.noreply.github.com>
2019-04-16 15:38:52 +0400

    Delete install_dependencies.py

--- install_dependencies.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.Popen("pip install pyexcel", shell=False)
-subprocess.Popen("pip install xlwt", shell=False)
-subprocess.Popen("pip install pyexcel-xls", shell=False)
-subprocess.Popen("pip install pyexcel-xlsx", shell=False)
